98c482e8ec8ab36f25f75dbae1da90ba3cde804e
brcdev-back <brcdesenvolvedor@gmail.com>
2026-01-13 22:12:36 -0300

    Update public.js

--- js/public.js
+++ js/public.js
@@ -1,22 +1,30 @@
-const WEBHOOK_URL = "https://discord.com/api/webhooks/1460801324567232700/lwkz3_C0fr6LJ2JigLapb9GfBuxe7YrLfRXk0ql0nCro64da8rtzTkbY4sez0V2pu-99";
+const WEBHOOK_INSCRICAO = "https://discord.com/api/webhooks/1460801324567232700/lwkz3_C0fr6LJ2JigLapb9GfBuxe7YrLfRXk0ql0nCro64da8rtzTkbY4sez0V2pu-99";
 
-const statusEdital = localStorage.getItem("edital") || "FECHADO";
+const status = localStorage.getItem("edital") || "FECHADO";
 document.getElementById("status-edital").innerText =
-  "Status do Edital: " + statusEdital;
+  "Status do Edital: " + status;
 
 function enviarInscricao() {
-  if (statusEdital !== "ABERTO") {
+  if (status !== "ABERTO") {
     alert("Edital fechado no momento.");
     return;
   }
 
-  fetch(WEBHOOK_URL, {
+  const nome = document.getElementById("nome").value;
+  const discord = document.getElementById("discord").value;
+  const idade = document.getElementById("idade").value;
+
+  fetch(WEBHOOK_INSCRICAO, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      content: "📥 **Nova solicitação de inscrição recebida**"
+      content:
+        "📥 **NOVA INSCRIÇÃO – GCM**\n" +
+        "Nome: " + nome + "\n" +
+        "Discord: " + discord + "\n" +
+        "Idade: " + idade
     })
   });
 
-  alert("Solicitação enviada. Aguarde contato no Discord.");
+  alert("Inscrição enviada com sucesso.");
 }
