21611bf11592f097ba2ffc8546546a0a5bda76f6
brcdev-back <brcdesenvolvedor@gmail.com>
2026-01-13 21:53:32 -0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,6 @@
-
+function addLog(texto) {
+  const logs = document.getElementById("logs");
+  const data = new Date().toLocaleString();
+  logs.innerHTML += `<p>[${data}] ${texto}</p>`;
+  logs.scrollTop = logs.scrollHeight;
+}
