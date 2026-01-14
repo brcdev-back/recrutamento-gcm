3eaf7900c03f047a25f4aeb38948a2300dfbf9bb
brcdev-back <brcdesenvolvedor@gmail.com>
2026-01-13 22:27:51 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,10 +1,8 @@
-const SENHA_ADMIN = "GCM2026";
-
 function login() {
-  const senha = document.getElementById("senha").value;
+  const id = document.getElementById("discordId").value;
 
-  if (senha === SENHA_ADMIN) {
-    localStorage.setItem("admin", "logado");
+  if (ADMINS.includes(id)) {
+    localStorage.setItem("auth", id);
     window.location.href = "admin.html";
   } else {
     alert("Acesso negado.");
