62cf18e09c4dc62d16cdeecdb6d9af039459e9ce
brcdev-back <brcdesenvolvedor@gmail.com>
2026-01-13 22:46:29 -0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,15 +1,21 @@
 const auth = localStorage.getItem("auth");
 
 if (!ADMINS.includes(auth)) {
+  alert("Acesso restrito à administração.");
   window.location.href = "login.html";
 }
 
 function abrir() {
   localStorage.setItem("edital", "aberto");
-  alert("Edital aberto");
+  alert("Edital ABERTO com sucesso.");
 }
 
 function fechar() {
   localStorage.setItem("edital", "fechado");
-  alert("Edital fechado");
+  alert("Edital FECHADO com sucesso.");
+}
+
+function logout() {
+  localStorage.removeItem("auth");
+  window.location.href = "login.html";
 }
